d37ee1361413de172eec064c447c6c0990293f5d
Wong Shun Min <wongsm7@hotmail.com>
2023-11-05 18:18:27 +0800

    Fix deploy error

--- src/components/SudokuBoard.tsx
+++ src/components/SudokuBoard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './SudokuBoard.scss'
 import SudokuCell from './SudokuCell'
 import { generateSudoku, isValidSudoku, solveSudoku } from '../utils/SudokuUtils'
